9a9d8d43f9c171d140b6f4105a4f6e6dfd91b9eb
Yama <pyama.ndiaye@gmail.com>
2022-11-16 00:36:15 -0500

    sadfsfg

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,6 @@ public class Main {
         tester.insert(1);
         tester.insert(5);
         System.out.println(tester);
-        tester.heapSort();
-        System.out.println(tester);
-        System.out.println();
 
-        Heap A = new Heap(10, "10 9 8 7 6 5 4 3 2 1");
-        System.out.println(A);
-        A.heapSort();
-        System.out.println(A);
     }
 }
